8969eabc937e53d92fa2a91e574aef572135caae
eduardo <eduardobaguespardo@gmail.com>
2016-11-03 18:47:27 +0100

    Ejercio 01

---
+++ Ejercicio01/src/Ejercicio01.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		int a,b,res=0,z;
+		
+		
+		System.out.println("Introduce un numero\n");
+		a = teclado.nextInt();
+		
+		System.out.println("Introduce otro numero\n");
+		b = teclado.nextInt();
+		
+		
+		if(a<=b){
+			
+			while(a+1<=b-1){
+				
+				res = res + (a+1);
+					
+					a++;
+			}
+			
+			System.out.println("La suma es "+res);	
+			}
+		
+		else { System.out.println("\nA debe ser menor o igual que B");}
+		
+		
+
+	}
+
+}
